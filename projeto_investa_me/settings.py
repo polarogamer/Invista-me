a1ddbf1649044a27ec878477c97b008239e684b7
polarogamer <5407922@aluno.edu.ma.gov.br>
2025-05-07 14:39:34 -0300

    commit settgins

--- projeto_investa_me/settings.py
+++ projeto_investa_me/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'projeto_investa_me.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'wulpfiiVedisJcNgQlbZykUzCRwCtceK',
+        'HOST': 'postgres.railway.internal',
+        'PORT': '5432',
     }
 }
 
